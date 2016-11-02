5610c2fff372d0c8308cb692349288cf415bf853
Kelsey Ruckert <klr324@psu.edu>
2016-11-02 13:57:38 -0400

    update main_script.R

--- RFILES/main_script.R
+++ RFILES/main_script.R
@@ -1,13 +1,19 @@
 # This main script calls all subscripts
+# Questions, contact Alexander Bakker: alexander.bakker@rws.nl
+# ================================================================================================
 
 # set working directory and empty environment
-setwd("~/Projects/SLR/deep_uncertainty/analysis/R-scripts")
+#setwd("~/Projects/SLR/deep_uncertainty/analysis/R-scripts")
 rm(list = ls())
 graphics.off()
 
-# load libraries for plotting and set plotting preferences
-source("set_theme.R")
-theme_set(theme_ali(base_size=20))
+# load libraries for plotting
+library(reshape2)
+library(lhs)
+library(rriskDistributions)
+library(ggplot2)
+library(grid)
+library(gridExtra)
 
 # Compare recent (local) assessments
 source("plot_recent_assessments.R")
@@ -15,3 +21,9 @@ source("plot_recent_assessments.R")
 # Analyse affect of different interpretation IPCC's likely' range
 source("likely_range.R")
 
+# Analyse affect of different interpretation on West-Antarctic ice sheet contribution to sea-level
+source("consensus_assessment.R")
+
+# NOTE: warnings will appear when plotting. This is because some rows contain NA's and therefore 
+#       these warnings can be ignored. For example, Perrette et al. 2013 does not project land 
+#       contribution to GSL.
